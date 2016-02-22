61d8b738d68c147cb34e6d5f7eb3882339f1ef18
hezw <410486365@qq.com>
2016-02-22 16:56:07 +0800

    搭建架构

--- src/cn/online/ssm/service/impl/UserService.java
+++ src/cn/online/ssm/service/impl/UserService.java
@@ -1,7 +1,12 @@
 package cn.online.ssm.service.impl;
 
+import cn.online.ssm.po.StudentPo;
+import cn.online.ssm.po.TeacherPo;
+
 /**
  * Created by hezw on 2016/2/22.
  */
 public interface UserService {
+    public TeacherPo TeaLogin() throws Exception;
+    public StudentPo StuLogin() throws Exception;
 }
--- src/cn/online/ssm/service/impl/UserServiceImpl.java
+++ src/cn/online/ssm/service/impl/UserServiceImpl.java
@@ -1,7 +1,19 @@
 package cn.online.ssm.service.impl;
 
+import cn.online.ssm.po.StudentPo;
+import cn.online.ssm.po.TeacherPo;
+
 /**
  * Created by hezw on 2016/2/22.
  */
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
+    @Override
+    public TeacherPo TeaLogin() throws Exception {
+        return null;
+    }
+
+    @Override
+    public StudentPo StuLogin() throws Exception {
+        return null;
+    }
 }
